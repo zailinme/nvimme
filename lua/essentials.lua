e4b155df71eeede1f3f62dd9c247c2957fd570d8
zailinme <108124536+zailinme@users.noreply.github.com>
2024-09-02 10:56:39 +0800

    Update essentials.lua
    添加注释信息

--- lua/essentials.lua
+++ lua/essentials.lua
@@ -2,53 +2,55 @@ local option = vim.opt
 local buffer = vim.b
 local global = vim.g
 
--- Globol Settings --
-option.showmode = false
-option.backspace = { "indent", "eol", "start" }
-option.tabstop = 4
-option.shiftwidth = 4
-option.expandtab = true
-option.shiftround = true
-option.autoindent = true
-option.smartindent = true
-option.number = true
-option.relativenumber = true
-option.wildmenu = true
-option.hlsearch = false
-option.ignorecase = true
-option.smartcase = true
-option.completeopt = { "menuone", "noselect" }
-option.cursorline = true
-option.termguicolors = true
-option.signcolumn = "yes"
-option.autoread = true
-option.title = true
-option.swapfile = false
-option.backup = false
-option.updatetime = 50
-option.mouse = "a"
-option.undofile = true
-option.undodir = vim.fn.expand('$HOME/.local/share/nvim/undo')
-option.exrc = true
-option.wrap = false
-option.splitright = true
+-- Global Settings --
+option.showmode = false -- 关闭编辑模式的显示，避免在状态栏上显示当前模式 (如INSERT, NORMAL等)
+option.backspace = { "indent", "eol", "start" } -- 配置退格键的行为，可以删除缩进、行尾和插入开始位置的字符
+option.tabstop = 4 -- 将一个tab字符显示为4个空格的宽度
+option.shiftwidth = 4 -- 自动缩进时使用4个空格的宽度
+option.expandtab = true -- 将tab键插入空格而不是实际的tab字符
+option.shiftround = true -- 缩进时，将缩进量四舍五入到shiftwidth的倍数
+option.autoindent = true -- 继承当前行的缩进到下一行
+option.smartindent = true -- 根据代码语法自动缩进新行
+option.number = true -- 显示行号
+option.relativenumber = false -- 显示相对行号（当前行号显示为实际行号，其他行显示为相对于当前行的行号）
+option.wildmenu = true -- 启用命令行补全增强（类似于IDE的自动补全）
+option.hlsearch = false -- 关闭搜索匹配高亮显示
+option.ignorecase = true -- 搜索时忽略大小写
+option.smartcase = true -- 当搜索模式中包含大写字母时，开启大小写敏感搜索
+option.completeopt = { "menu", "menuone" } -- 配置自动补全选项，使补全菜单更友好
+option.cursorline = true -- 高亮显示当前行
+option.termguicolors = true -- 启用终端的真彩色支持
+option.signcolumn = "yes" -- 始终显示标记列
+option.autoread = true -- 当文件在外部被修改时，自动重新读取文件
+option.title = true -- 显示窗口标题
+option.swapfile = false -- 禁用交换文件（swap file）
+option.backup = false -- 禁用备份文件
+option.updatetime = 50 -- 更改触发事件前的等待时间（以毫秒为单位），默认4000毫秒
+option.mouse = "n" -- 启用普通模式下的鼠标支持
+option.undofile = true -- 启用持久化撤销
+option.undodir = vim.fn.expand('$HOME/.local/share/nvim/undo') -- 设置撤销文件的保存目录
+option.exrc = true -- 允许本地vimrc文件
+option.wrap = false -- 禁用自动换行
+option.splitright = true -- 新建垂直分屏时，将新窗口放在右侧
 
 -- Buffer Settings --
-buffer.fileenconding = "utf-8"
+buffer.fileencoding = "utf-8" -- 设置文件编码为UTF-8
 
 -- Global Settings --
-global.mapleader = " "
+global.mapleader = " " -- 设置leader键为空格键
 
 -- Key mappings --
-vim.keymap.set({ "n", "i", "v" }, "<Left>", "<Nop>")
-vim.keymap.set({ "n", "i", "v" }, "<Right>", "<Nop>")
-vim.keymap.set({ "n", "i", "v" }, "<Up>", "<Nop>")
-vim.keymap.set({ "n", "i", "v" }, "<Down>", "<Nop>")
+-- vim.keymap.set({ "n", "i", "v" }, "<Left>", "<Nop>") -- 禁用左箭头键
+-- vim.keymap.set({ "n", "i", "v" }, "<Right>", "<Nop>") -- 禁用右箭头键
+-- vim.keymap.set({ "n", "i", "v" }, "<Up>", "<Nop>") -- 禁用上箭头键
+-- vim.keymap.set({ "n", "i", "v" }, "<Down>", "<Nop>") -- 禁用下箭头键
 
-vim.keymap.set("n", "<A-Tab>", "<cmd>bNext<CR>") 
-vim.keymap.set("n", "<leader>bc", "<cmd>bd<CR>")
+vim.keymap.set("n", "<A-Tab>", "<cmd>bNext<CR>") -- 在普通模式下，按Alt+Tab切换到下一个缓冲区
+vim.keymap.set("n", "<leader>bc", "<cmd>bd<CR>") -- 在普通模式下，按leader键加bc关闭当前缓冲区
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv") -- 在可视模式下，将选中内容向下移动一行并重新选中
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv") -- 在可视模式下，将选中内容向上移动一行并重新选中
 
-vim.keymap.set({ "v", "n" }, "<leader>y", "\"+y")
+-- vim.keymap.set({ "v", "n" }, "<leader>y", "\"+y") -- 在可视模式和普通模式下，按leader键加y复制内容到系统剪贴板
+
+
